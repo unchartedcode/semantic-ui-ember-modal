9e6325474c90aacef66ba686c8218d0ec77ae0b5
Nathan Palmer <nathan@nathanpalmer.com>
2016-01-08 18:48:51 -0500

    Updated modal/ paths to semantic-ui-ember-modal/

--- addon/initializers/modal.js
+++ addon/initializers/modal.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 import UiModal from 'semantic-ui-ember/components/ui-modal';
-import SemanticUiActionsMixin from 'modal/mixins/semantic-ui-actions';
-import SemanticUiDefaultsMixin from 'modal/mixins/semantic-ui-defaults';
-import ComponentMixin from 'modal/mixins/component';
+import SemanticUiActionsMixin from 'semantic-ui-ember-modal/mixins/semantic-ui-actions';
+import SemanticUiDefaultsMixin from 'semantic-ui-ember-modal/mixins/semantic-ui-defaults';
+import ComponentMixin from 'semantic-ui-ember-modal/mixins/component';
 
 export function initialize(/* application */) {
   UiModal.reopen(SemanticUiDefaultsMixin, SemanticUiActionsMixin);
