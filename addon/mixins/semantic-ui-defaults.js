7704aa99efdc89f4acaf13e4363b092cb164d1b7
Nathan Palmer <nathan@nathanpalmer.com>
2016-03-08 09:31:10 -0500

    Added observeChanges as a default so the position will update correctly

--- addon/mixins/semantic-ui-defaults.js
+++ addon/mixins/semantic-ui-defaults.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   attributeBindings: 'name',
-  transition: 'horizontal flip'
+  transition: 'horizontal flip',
+  observeChanges: true
 });
