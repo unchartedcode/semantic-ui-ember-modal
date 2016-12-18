d84a76969adc0d7525fbf110e3534d7129d0cbcc
Aaron Hansen <aaronbhansen@gmail.com>
2016-12-17 20:48:02 -0700

    changing args to options to support ember spread

--- addon/initializers/modal.js
+++ addon/initializers/modal.js
@@ -3,9 +3,10 @@ import UiModal from 'semantic-ui-ember/components/ui-modal';
 import SemanticUiActionsMixin from 'semantic-ui-ember-modal/mixins/semantic-ui-actions';
 import SemanticUiDefaultsMixin from 'semantic-ui-ember-modal/mixins/semantic-ui-defaults';
 import ComponentMixin from 'semantic-ui-ember-modal/mixins/component';
+import SpreadMixin from 'ember-spread';
 
 export function initialize(/* application */) {
-  UiModal.reopen(SemanticUiDefaultsMixin, SemanticUiActionsMixin);
+  UiModal.reopen(SemanticUiDefaultsMixin, SemanticUiActionsMixin, SpreadMixin);
   Ember.Component.reopen(ComponentMixin);
   Ember.Controller.reopen(ComponentMixin);
 }
--- addon/mixins/modal-actions.js
+++ addon/mixins/modal-actions.js
@@ -5,17 +5,15 @@ export default Ember.Mixin.create({
     open_modal: function() {
       let args = Array.prototype.slice.call(arguments);
       let name = args.shift();
+      let options = args.shift();
       let controller = this.controllerFor('application');
 
       // Wire up the close event
-      if (args.length === 0) {
-        args.push({});
+      if (options == null) {
+        options = {};
       }
-      if (args[0] === null) {
-        args[0] = {};
-      }
-      args[0].modalId = Ember.guidFor(name);
-      args[0].close_modal = function(modalId) {
+      options.modalId = Ember.guidFor(name);
+      options.close_modal = function(modalId) {
         if (controller.get('isDestroyed') || controller.get('isDestroying')) { return; }
         controller.set('modals', controller.get('modals').rejectBy('id', modalId));
       };
@@ -24,9 +22,9 @@ export default Ember.Mixin.create({
         controller.set('modals', Ember.A());
       }
       controller.get('modals').pushObject({
-        id: args[0].modalId,
+        id: options.modalId,
         name: name,
-        args: args || []
+        options: options
       });
     }
   }
